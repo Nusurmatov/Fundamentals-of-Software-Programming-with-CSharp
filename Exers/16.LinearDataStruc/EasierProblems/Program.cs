a4b45349c199b51c632cfed5ed72cf36d2aa227f
Nusurmatov Husniddin <NusurmatovPro@gmail.com>
2023-01-05 21:14:13 +0500

    LinearDatastruc Exer 4,5 are done

--- Exers/16.LinearDataStruc/EasierProblems/Program.cs
+++ Exers/16.LinearDataStruc/EasierProblems/Program.cs
@@ -1,23 +1,27 @@
-﻿/* Ex4 - Problem Statement:
-Write a method that finds the longest subsequence of equal numbers
-in a given List<int> and returns the result as new List<int>. Write a
-program to test whether the method works correctly.
+﻿/* Ex5 - Problem Statement:
+Write a program, which removes all negative numbers from a sequence. 
+Example: array = {19, -10, 12, -6, -3, 34, -2, 5} -> {19, 12, 34, 5}
 */
 
 Random random = new Random();
-List<int> intList = GenerateRandomList(random.Next(1, 17), random.Next(1, 7), random.Next(7, 10));
+List<int> intList = GenerateRandomList(random.Next(7, 17), random.Next(-17, -1), random.Next(1, 17));
+PrintList(intList);
+Console.Write(" -> ");
 
- void PrintList<T>(List<T> items)
+var removeNegatives = intList.FindAll(n => (n >= 0));
+PrintList(removeNegatives);
+
+void PrintList<T>(List<T> items)
 {
     Console.Write("{ ");
     foreach (var item in items)
     {
         Console.Write($"{item} ");
     }
-    Console.WriteLine("}");
+    Console.Write("}");
 }
     
- List<int> GenerateRandomList(int length, int lowerBound, int upperBound)
+List<int> GenerateRandomList(int length, int lowerBound, int upperBound)
 {
     var result = new List<int>();
 
@@ -29,14 +33,8 @@ List<int> intList = GenerateRandomList(random.Next(1, 17), random.Next(1, 7), ra
     return result;
 }
 
-/* Input/Output:
-Enter a positeve integer or nothing to quit: 7
-Enter a positeve integer or nothing to quit: 79
-Enter a positeve integer or nothing to quit: 1654
-Enter a positeve integer or nothing to quit: 4
-Enter a positeve integer or nothing to quit: 654
-Enter a positeve integer or nothing to quit: 7
-Enter a positeve integer or nothing to quit: 98
-Enter a positeve integer or nothing to quit: 
-Entered integers after Ascending Sorting: 4 7 7 79 98 654 1654 
+/* Output:
+{ -8 7 5 10 3 10 -1 6 -6 -3 -2 4 } -> { 7 5 10 3 10 6 4 }
+{ 8 -7 7 -5 -7 1 -8 4 8 -6 1 3 } -> { 8 7 1 4 8 1 3 }
+{ 13 7 6 2 5 -1 2 -3 -2 0 2 -1 -1 12 11 -2 } -> { 13 7 6 2 5 2 0 2 12 11 }
 */
