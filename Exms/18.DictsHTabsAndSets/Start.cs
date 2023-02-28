22a3586010101ff812852d5f117c448c1af41a78
Husniddin <NusurmatovPro@gmail.com>
2023-02-28 09:57:26 +0500

    continue video practice

--- Exms/18.DictsHTabsAndSets/Start.cs
+++ Exms/18.DictsHTabsAndSets/Start.cs
@@ -7,6 +7,7 @@ public class Start
         
         Console.Clear();
         Console.Write("Enter sequence of numbers seperated with one space: ");
+        Console.WriteLine($"\n{"-".PadLeft(7)} Math Operations {"-".PadRight(7)}");
         numList = Console.ReadLine()?.Split().Select(double.Parse).ToList();
         
         // Math operations: Min, Max, Sum, Average
@@ -16,7 +17,7 @@ public class Start
         Console.WriteLine($"Sum -> {numList.Sum()}");
         Console.WriteLine($"Average ->f {numList.Average():0.##}\n");
 
-        // Converting Collections:
+        // Converting Collections: ToArray, ToList, ToDictionary, ToCharArray
         string[] fullNames = {"Nusurmatov Husniddin", "Inomjonov Jahongir"};
         Console.WriteLine(string.Join(", ", fullNames));
         string[] names = fullNames.Select(x => x.Split()[0]).ToArray();
@@ -25,9 +26,23 @@ public class Start
         char[] spelledSurname = names.Last().ToCharArray();
         Console.WriteLine("Spellecd surname: {0}", string.Join(", ", spelledSurname));
 
-        Dictionary<string, string> dict = fullNames.ToDictionary(x => x.Split().First());
+        var dict = fullNames.Select(x => x.Split())
+                            .ToDictionary(p => p.First(), p => p.Last());
+        Console.WriteLine("Surname and Name: {0}\n", string.Join(", ", dict));
     
-        // Sorting Collections: 
+        // Sorting Collections: OrderBy, ThenBy
+        var nums = new List <int> { 5, 7, 10, -5}; 
+        var strNums = new List <string> { "5", "7", "10", "-5"}; 
+        Console.WriteLine("Number list (ascending): {0}", string.Join(", ", nums.OrderBy(x => x)));
+        Console.WriteLine("Number list (descending): {0}", string.Join(", ", nums.OrderByDescending(x => x)));
+        Console.WriteLine("String number list (ascending): {0}", string.Join(", ", nums.OrderBy(x => x)));
+        Console.WriteLine("String number list (descending): {0}\n", string.Join(", ", nums.OrderByDescending(x => x)));
+
+        var products = new Dictionary<int, string>();
+
+        var orderedProducts = products.OrderBy(pair => pair.Value)
+                                      .ThenBy(pair => pair.Key)
+                                      .ToDictionary(pair => pair.Key, pair => pair.Value);
     }
 
     private static List<double> GenerateIEnumerable(int size = 7, int lowerBound = -100, int upperBound = 100)
